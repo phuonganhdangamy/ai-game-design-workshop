f03896134067e8ad16f86b2bdf7875f2f84d57a5
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-09-27 12:19:19 -0400

    add mutate function

--- proc-gen/procGen.py
+++ proc-gen/procGen.py
@@ -1,5 +1,11 @@
 from game import Game
 from autoplay import solve
+from random import random
+
+
+def mutate(prob: float) -> float:
+    prob += (random() - 0.5) * 0.1
+    return prob if prob <= 1 and prob >= 0 else (1 if prob > 1 else 0)
 
 
 def procGen(width: int, height: int, desiredMovesToWin: int, allowance: int):
